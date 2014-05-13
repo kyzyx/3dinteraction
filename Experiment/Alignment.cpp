7ab341bbdac2fac7b72137af0274349f7ccb1c51
Edward Zhang <edzhang@cs.washington.edu>
2014-05-12 19:16:37 -0700

    Implemented least squares solver

--- Experiment/Alignment.cpp
+++ Experiment/Alignment.cpp
@@ -1,6 +1,7 @@
 #include "Alignment.h"
+#include "Eigen/Dense"
 
-
+using namespace Eigen;
 Alignment::Alignment(void)
 {
 }
@@ -20,6 +21,41 @@ Xform Alignment::solveRansac() {
 	return Xform::Identity();
 }
 Xform Alignment::solveLeastSquares() {
-	// FIXME
-	return Xform::Identity();
+	int neq = 3*inputPoints.size();
+
+	MatrixXd data;
+	data.resize(neq, 12);
+	data.fill(0);
+
+	int row = 0;
+	for (int i = 0; i < inputPoints.size(); ++i) {
+		for (int j = 0; j < 3; ++j) {
+			data(row, j*4 + 0) = inputPoints[i].x();
+			data(row, j*4 + 1) = inputPoints[i].y();
+			data(row, j*4 + 2) = inputPoints[i].z();
+			data(row, j*4 + 3) = 1;
+			++row;
+		}
+	}
+
+	VectorXd coords;
+	coords.resize(neq);
+	row = 0;
+	for (int i = 0; i < outputPoints.size(); ++i) {
+		coords(row++) = outputPoints[i].x();
+		coords(row++) = outputPoints[i].y();
+		coords(row++) = outputPoints[i].z();
+	}
+
+	VectorXd result = data.colPivHouseholderQr().solve(coords);
+	Matrix<double, 4, 4> transform;
+	transform.fill(0);
+	transform(3,3) = 1;
+	for (int i = 0; i < 3; ++i) {
+		for (int j = 0; j < 4; ++j) {
+			transform(i,j) = result(4*i + j);
+		}
+	}
+
+	return Xform(transform);
 }
