ea2d50d9b8b30ed3fbfaa2a822a43a4a7b9cb919
Edward Zhang <edzhang@cs.washington.edu>
2014-05-27 18:04:31 -0700

    Enable selection for leap

--- Experiment/AdjustableInterface.cpp
+++ Experiment/AdjustableInterface.cpp
@@ -9,6 +9,10 @@ AdjustableInterface::~AdjustableInterface(void)
 InputStatus AdjustableInterface::_getStatus (void) {
 	status = input->getStatus();
 	status.pos = t + status.pos;
+	if (selected) {
+		status.setFlag(InputStatus::INPUTFLAG_SELECT);
+		selected = false;
+	}
 	return status;
 }
 
@@ -35,5 +39,12 @@ void AdjustableInterface::OnKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode) {
 		case SDLK_c: 
 			t.y() -= 1;
 			break;
+		case SDLK_SPACE:
+			selected = true;
+			break;
 	}
+}
+
+void AdjustableInterface::OnKeyUp(SDLKey sym, SDLMod mod, Uint16 unicode) {
+	if (sym == SDLK_SPACE) selected = false;
 }
--- Experiment/AdjustableInterface.h
+++ Experiment/AdjustableInterface.h
@@ -5,15 +5,17 @@ class AdjustableInterface :
 	public InputInterface, public SDLHandler
 {
 public:
-	AdjustableInterface(InputInterface* inputinterface, App* app) : SDLHandler(app), input(inputinterface), t(Eigen::Vector3d::Zero()) {;}
+	AdjustableInterface(InputInterface* inputinterface, App* app) : SDLHandler(app), input(inputinterface), t(Eigen::Vector3d::Zero()), selected(false) {;}
 	~AdjustableInterface(void);
 	virtual void update() { input->update(); }
 	virtual void OnKeyDown(SDLKey sym, SDLMod mod, Uint16 unicode);
+	virtual void OnKeyUp(SDLKey sym, SDLMod mod, Uint16 unicode);
 protected:
 	virtual InputStatus _getStatus(void);
 private:
 	AdjustableInterface();
 	Eigen::Vector3d t;
 	InputInterface* input;
+	bool selected;
 };
 
